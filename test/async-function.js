7d4279fc002f5e4bc2965e6391fb97789183793a
Titus Wormer <tituswormer@gmail.com>
2021-07-29 14:09:06 +0200

    Revert

--- test/async-function.js
+++ test/async-function.js
@@ -8,26 +8,26 @@ import {unified} from '../index.js'
 
 test('async function transformer () {}', (t) => {
   const givenFile = new VFile('alpha')
-  /** @type {Node} */
   const givenNode = {type: 'bravo'}
-  /** @type {Node} */
   const modifiedNode = {type: 'charlie'}
 
   t.plan(5)
 
   unified()
     .use(() => async function () {})
-    .use(() => async (tree, file) => {
-      t.equal(tree, givenNode, 'passes correct tree to an async function')
-      t.equal(file, givenFile, 'passes correct file to an async function')
-      return modifiedNode
-    })
     .use(
+      // Note: TS JS doesn’t understand the `Promise<undefined>` w/o explicit type.
+      /** @type {import('../index.js').Plugin<[]>} */
       () =>
         async function () {
           return undefined
         }
     )
+    .use(() => async (tree, file) => {
+      t.equal(tree, givenNode, 'passes correct tree to an async function')
+      t.equal(file, givenFile, 'passes correct file to an async function')
+      return modifiedNode
+    })
     .run(givenNode, givenFile, (error, tree, file) => {
       t.error(error, 'should’t fail')
       t.equal(tree, modifiedNode, 'passes given tree to `done`')
